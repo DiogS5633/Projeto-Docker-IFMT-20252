0753ad8b039b62f35f787647282b90842c97e360
DiogS5633 <diogo.mendoza@estudante.ifmt.edu.br>
2025-11-03 15:45:57 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,27 @@
-# Nome da rede Docker interna
 variable "rede_nome" {
-  description = "Nome da rede interna Docker"
+  description = "Nome da rede Docker interna"
   default     = "rede_interna"
 }
 
-# Lista de containers de aplicação
-variable "containers" {
-  description = "Lista de containers das aplicações"
-  default     = ["app1", "app2", "app3"]
+variable "porta_externa" {
+  description = "Porta exposta do proxy reverso"
+  default     = 80
 }
 
-# Imagem Docker usada no proxy reverso
 variable "nginx_image" {
-  description = "Imagem Docker para o proxy reverso"
+  description = "Imagem Docker do Nginx proxy"
   default     = "nginx:latest"
+}
+
+variable "containers" {
+  description = "Lista de aplicações e réplicas"
+  type = list(object({
+    name     = string
+    replicas = number
+  }))
+  default = [
+    { name = "app1", replicas = 2 },
+    { name = "app2", replicas = 2 },
+    { name = "app3", replicas = 2 }
+  ]
 }
